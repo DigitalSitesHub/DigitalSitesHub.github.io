3eaaa9c2c7387f861eee105d5441ef6efd9e7544
DigitalSitesHub <anya24426@gmail.com>
2026-01-25 14:45:28 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,37 @@
-const CACHE_NAME = "digital-siteshub-v1";
-const urlsToCache = ["./"];
+// Service Worker for Digital Sites Hub
+const CACHE_NAME = 'digital-sites-hub-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/icon-192.png',
+    '/icon-512.png',
+    '/manifest.json'
+];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
-  self.skipWaiting();
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(
-      response => response || fetch(event.request)
-    )
-  );
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
 });
